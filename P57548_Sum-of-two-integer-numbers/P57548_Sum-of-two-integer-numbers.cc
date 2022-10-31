c756aefec703e42013ff8c3ccd0dd3acb16c704d
IPG2004 <alu0101543345@ull.edu.es>
2022-10-31 18:05:10 +0000

    Práctica_5

---
+++ P57548_Sum-of-two-integer-numbers/P57548_Sum-of-two-integer-numbers.cc
@@ -0,0 +1,21 @@
+/**
+  * Universidad de La Laguna
+  * Escuela Superior de Ingeniería y Tecnología
+  * Grado en Ingeniería Informática
+  *
+  *@file P57548_Sum of two integer numbers
+  *@author Izan Pérez González alu0101543345@ull.edu.es
+  *@date 31 Oct 2022
+  *@bug There are no known bugs
+  */
+
+#include <iostream>
+
+int main() {
+  //std::cout << "Introduzca dos numeros enteros para que sean sumados << std::endl;
+  int numero_1, numero_2;
+  std::cin >> numero_1 >> numero_2;
+  std::cout << numero_1 + numero_2 << std::endl;
+
+  return 0;
+}
